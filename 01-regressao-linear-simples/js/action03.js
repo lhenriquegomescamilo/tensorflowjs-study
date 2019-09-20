2a6d15a7ec055f43d46a432ca9cbf644bc8d5f86
lcamilo <lhenriquegomescamilo@gmail.com>
2019-09-19 23:51:20 -0300

    Criando rede para extrair a sequencia de regressao linear

---
+++ 01-regressao-linear-simples/js/action03.js
@@ -0,0 +1,49 @@
+﻿$('#result').text('');
+
+function exibir(str = '') {
+    $('#result').text(str);
+}
+
+function executar() {
+    exibir('...processando.');
+    let txt = '';
+
+    const model = tf.sequential({
+        layers: [
+            tf.layers.dense({inputShape: [1], units: 1})
+        ]
+    });
+    model.compile({
+        optimizer: 'sgd',
+        loss: 'meanSquaredError',
+        metrics: ['accuracy']
+    });
+
+    const eixoX = tf.tensor([1, 2, 3, 4], [4, 1]);
+    const eixoY = tf.tensor([[10], [20], [30], [40]]);
+    const input = tf.tensor([5, 6, 7], [3, 1]);
+
+    model.fit(eixoX, eixoY, {epochs: 500}).then(() => {
+        const output = model.predict(input).dataSync().map(v => Math.ceil(v));
+        const tensorOutput = tf.tensor(output);
+
+        txt += 'Regressão Linear Simples com Rede Neural:\n';
+        txt += 'TREINAMENTO:\n';
+        txt += eixoY.flatten().toString() + '\n\n';
+        txt += eixoY.flatten().toString() + '\n\n';
+        txt += 'ENTRADA:\n';
+        txt += input.flatten().toString() + '\n\n';
+        txt += 'SAIDA:\n\n';
+        txt += tensorOutput.toString();
+        exibir(txt);
+
+    });
+}
+
+// function converteArray(array) {
+//     let result = [];
+//     for (let i = 0; i < array.length; i++) {
+//         result.push(Math.ceil(array[i]));
+//     }
+//     return result;
+// }
